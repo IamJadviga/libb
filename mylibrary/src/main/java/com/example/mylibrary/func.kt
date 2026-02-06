efac5452d7810aa39df5b5cc9247f6b190c9a810
IamJadviga <Gorevanastya1999@gmail.com>
2026-02-06 13:42:06 +0300

    zzzz

---
+++ mylibrary/src/main/java/com/example/mylibrary/func.kt
@@ -0,0 +1,13 @@
+package com.example.mylibrary
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Hi(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
